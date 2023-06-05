d234ce062bce61de31701bd21ffb8ee2363e7737
川田広輝 <f13bc005@gmail.com>
2023-06-06 07:39:02 +0900

    アプリケーションクラス追加 ネット通信許可

---
+++ app/src/main/java/com/example/pokemondictionary/PokemonDictionaryApplication.kt
@@ -0,0 +1,8 @@
+package com.example.pokemondictionary
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokemonDictionaryApplication : Application() {
+}
