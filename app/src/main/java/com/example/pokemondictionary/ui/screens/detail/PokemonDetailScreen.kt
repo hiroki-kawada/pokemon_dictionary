e4d706ecbf17dae5f83c1533798b435d5db321f1
川田広輝 <f13bc005@gmail.com>
2023-06-08 07:52:52 +0900

    UI関連のファイル作成

---
+++ app/src/main/java/com/example/pokemondictionary/ui/screens/detail/PokemonDetailScreen.kt
@@ -0,0 +1,7 @@
+package com.example.pokemondictionary.ui.screens.detail
+
+/**
+ * 詳細画面
+ */
+class PokemonDetailScreen {
+}
---
+++ app/src/main/java/com/example/pokemondictionary/ui/screens/list/PokemonListScreen.kt
@@ -0,0 +1,7 @@
+package com.example.pokemondictionary.ui.screens.list
+
+/**
+ * 一覧画面
+ */
+class PokemonListScreen {
+}
